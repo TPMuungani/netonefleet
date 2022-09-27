983e543b7ce723ecd2b609f2a35362017a698038
TPMuungani <tpmuungani@gmail.com>
2022-09-27 10:58:37 +0200

    NetOneFleetApplication

--- src/main/java/com/example/netonefleet/FleetApplicationController.java
+++ src/main/java/com/example/netonefleet/FleetApplicationController.java
@@ -1,6 +1,11 @@
 package com.example.netonefleet;
 
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class FleetApplicationController {
 
-
+    public String homePage(){
+        return "homepage";
+    }
 }
