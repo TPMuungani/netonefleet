42df58e11cb792267b564c7a02fbc62629ef1d9b
TPMuungani <tpmuungani@gmail.com>
2022-09-27 10:20:07 +0200

    NetOneFleetApplication

---
+++ src/main/java/com/example/netonefleet/NetonefleetApplication.java
@@ -0,0 +1,13 @@
+package com.example.netonefleet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetonefleetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NetonefleetApplication.class, args);
+    }
+
+}
